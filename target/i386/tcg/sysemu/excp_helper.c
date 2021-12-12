1e0b63faf650d0272b5af316b3faabc0a271e341
Siyuan <siyuanc3@illinois.edu>
2021-12-12 11:38:08 -0600

    add ADDR_MASK to correct pte extraction

--- target/i386/tcg/sysemu/excp_helper.c
+++ target/i386/tcg/sysemu/excp_helper.c
@@ -91,34 +91,71 @@ static uint64_t gen_has(uint64_t vpn, uint64_t size) {
     return hash;
 }
 
+// #define PAGE_HEADER_MASK (0xffff000000000000)
+// #define PAGE_TAIL_MASK_4KB (0xfff)
+// #define PAGE_TAIL_MASK_2MB (0x1fffff)
+// #define PAGE_TAIL_MASK_1GB (0x3fffffff)
+
+
+// #define PAGE_SHIFT_4KB (12)
+// #define PAGE_SHIFT_2MB (21)
+// #define PAGE_SHIFT_1GB (30)
+
+// #define PAGE_SIZE_4KB (1UL << PAGE_SHIFT_4KB)
+// #define PAGE_SIZE_2MB (1UL << PAGE_SHIFT_2MB)
+// #define PAGE_SIZE_1GB (1UL << PAGE_SHIFT_1GB)
+
+// #define ADDR_TO_PAGE_NUM_4KB(x)   (((x) & ~ PAGE_HEADER_MASK) >> PAGE_SHIFT_4KB)
+// #define ADDR_TO_PAGE_NUM_2MB(x)   (((x) & ~ PAGE_HEADER_MASK) >> PAGE_SHIFT_2MB)
+// #define ADDR_TO_PAGE_NUM_1GB(x)   (((x) & ~ PAGE_HEADER_MASK) >> PAGE_SHIFT_1GB)
+
+// #define ADDR_TO_OFFSET_4KB(x)   ((x) & PAGE_TAIL_MASK_4KB)
+// #define ADDR_TO_OFFSET_2MB(x)   ((x) &     PAGE_TAIL_MASK_2MB)
+// #define ADDR_TO_OFFSET_1GB(x)   ((x) &   PAGE_TAIL_MASK_1GB)
+
+// #define PAGE_NUM_TO_ADDR_4KB(x)   (((hwaddr)x) << PAGE_SHIFT_4KB)
+// #define PAGE_NUM_TO_ADDR_2MB(x)   (((hwaddr)x) << PAGE_SHIFT_2MB)
+// #define PAGE_NUM_TO_ADDR_1GB(x)   (((hwaddr)x) << PAGE_SHIFT_1GB)
+
 #define PAGE_HEADER_MASK (0xffff000000000000)
 #define PAGE_TAIL_MASK_4KB (0xfff)
 #define PAGE_TAIL_MASK_2MB (0x1fffff)
 #define PAGE_TAIL_MASK_1GB (0x3fffffff)
-
+#define PAGE_TAIL_MASK_512GB (0x7fffffffff)
 
 #define PAGE_SHIFT_4KB (12)
 #define PAGE_SHIFT_2MB (21)
 #define PAGE_SHIFT_1GB (30)
+#define PAGE_SHIFT_512GB (39)
+
 
 #define PAGE_SIZE_4KB (1UL << PAGE_SHIFT_4KB)
 #define PAGE_SIZE_2MB (1UL << PAGE_SHIFT_2MB)
 #define PAGE_SIZE_1GB (1UL << PAGE_SHIFT_1GB)
+#define PAGE_SIZE_512GB (1UL << PAGE_SHIFT_512GB)
 
-#define ADDR_TO_PAGE_NUM_4KB(x)   (((x) & ~ PAGE_HEADER_MASK) >> PAGE_SHIFT_4KB)
-#define ADDR_TO_PAGE_NUM_2MB(x)   (((x) & ~ PAGE_HEADER_MASK) >> PAGE_SHIFT_2MB)
-#define ADDR_TO_PAGE_NUM_1GB(x)   (((x) & ~ PAGE_HEADER_MASK) >> PAGE_SHIFT_1GB)
+#define ADDR_REMOVE_OFFSET_4KB(x)   ((x) & ~PAGE_TAIL_MASK_4KB)
+#define ADDR_REMOVE_OFFSET_2MB(x)   ((x) & ~PAGE_TAIL_MASK_2MB)
+#define ADDR_REMOVE_OFFSET_1GB(x)   ((x) & ~PAGE_TAIL_MASK_1GB)
 
 #define ADDR_TO_OFFSET_4KB(x)   ((x) & PAGE_TAIL_MASK_4KB)
-#define ADDR_TO_OFFSET_2MB(x)   ((x) &     PAGE_TAIL_MASK_2MB)
-#define ADDR_TO_OFFSET_1GB(x)   ((x) &   PAGE_TAIL_MASK_1GB)
+#define ADDR_TO_OFFSET_2MB(x)   ((x) & PAGE_TAIL_MASK_2MB)
+#define ADDR_TO_OFFSET_1GB(x)   ((x) & PAGE_TAIL_MASK_1GB)
 
-#define PAGE_NUM_TO_ADDR_4KB(x)   (((hwaddr)x) << PAGE_SHIFT_4KB)
-#define PAGE_NUM_TO_ADDR_2MB(x)   (((hwaddr)x) << PAGE_SHIFT_2MB)
-#define PAGE_NUM_TO_ADDR_1GB(x)   (((hwaddr)x) << PAGE_SHIFT_1GB)
+#define ADDR_TO_PAGE_NUM_4KB(x)   ((ADDR_REMOVE_OFFSET_4KB(x)) >> PAGE_SHIFT_4KB)
+#define ADDR_TO_PAGE_NUM_2MB(x)   ((ADDR_REMOVE_OFFSET_2MB(x)) >> PAGE_SHIFT_2MB)
+#define ADDR_TO_PAGE_NUM_1GB(x)   ((ADDR_REMOVE_OFFSET_1GB(x)) >> PAGE_SHIFT_1GB)
 
-#define HPT_SIZE_MASK (0xfff)      /* max hpt size = 16383*/
-#define HPT_SIZE_HIDDEN_BITS (4)    /* 16 * cr3[0:11] for number of entries */
+
+#define PAGE_NUM_TO_ADDR_4KB(x)   (((uint64_t) x) << PAGE_SHIFT_4KB)
+#define PAGE_NUM_TO_ADDR_2MB(x)   (((uint64_t) x) << PAGE_SHIFT_2MB)
+#define PAGE_NUM_TO_ADDR_1GB(x)   (((uint64_t) x) << PAGE_SHIFT_1GB)
+
+
+#define HPT_SIZE_MASK (0xfff)      	/* 16 * cr3[0:11] for number of entries */
+#define HPT_SIZE_HIDDEN_BITS (4)    
+// #define HPT_CR3_TO_NUM_ENTRIES(cr3) ((((uint64_t) cr3 ) & HPT_SIZE_MASK) << HPT_SIZE_HIDDEN_BITS)
+#define HPT_NUM_ENTRIES_TO_CR3(size) (((uint64_t) cr3 ) >> HPT_SIZE_HIDDEN_BITS)
 #define HPT_BASE_MASK (~(HPT_SIZE_MASK))
 #define GET_HPT_SIZE(cr3) ((((uint64_t) cr3) & HPT_SIZE_MASK ) << HPT_SIZE_HIDDEN_BITS)
 #define GET_HPT_BASE(cr3) (((uint64_t) cr3) & HPT_BASE_MASK )
@@ -157,26 +194,28 @@ static int mmu_translate(CPUState *cs, hwaddr addr, MMUTranslateFunc get_hphys_f
      *      size of the page table obtained by 
      * 
      *  TODO:
-     *      translation only for 1GB right now
+     *      translation only for 2MB right now
      */
     uint64_t size = GET_HPT_SIZE(cr3);
     uint64_t hash = gen_has(ADDR_TO_PAGE_NUM_2MB(addr) , size);
 
-    qemu_log_mask(CPU_LOG_MMU, "    Translate: hash=0x%lx ppn =0x%lx size0x%lx\n", hash, ADDR_TO_PAGE_NUM_2MB(addr), size);
+    qemu_log_mask(CPU_LOG_MMU, "    Translate: hash=0x%lx vpn =0x%lx size=0x%lx\n", hash, ADDR_TO_PAGE_NUM_2MB(addr), size);
     
     hwaddr pte_addr = GET_HPT_BASE(cr3)                /* page table base */
                             + (hash << 3);           /*  offset; */
+	qemu_log_mask(CPU_LOG_MMU, "    Translate: load from 0x%016lx\n", pte_addr);
     pte_addr = GET_HPHYS(cs, pte_addr, MMU_DATA_STORE, NULL);
     uint64_t pte = x86_ldq_phys(cs, pte_addr);
 
 
-    qemu_log_mask(CPU_LOG_MMU, "    Translate: pte=0x%16lx\n", pte);
+    qemu_log_mask(CPU_LOG_MMU, "    Translate: pte=0x%016lx\n", pte);
 
     uint64_t ptep = PG_NX_MASK | PG_USER_MASK | PG_RW_MASK;
     ptep &= pte;
     *page_size = PAGE_SIZE_2MB;
     /*  */
     if (!(pte & PG_PRESENT_MASK)) {
+		qemu_log_mask(CPU_LOG_MMU, "    fault triggered. Page not Present!\n");
         goto do_fault;
     }
 
@@ -265,6 +304,7 @@ static int mmu_translate(CPUState *cs, hwaddr addr, MMUTranslateFunc get_hphys_f
     // pte &= PG_ADDRESS_MASK & ~(*page_size - 1);
     page_offset = ADDR_TO_OFFSET_2MB(addr);
     pte = PAGE_NUM_TO_ADDR_2MB(ADDR_TO_PAGE_NUM_2MB(pte));
+    pte = pte & PG_ADDRESS_MASK;
     *xlat = GET_HPHYS(cs, pte + page_offset, is_write1, prot);
     return PG_ERROR_OK;
 
